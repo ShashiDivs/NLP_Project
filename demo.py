03b99ca5df2c1803b5cffe1dd8456436477d2cbc
Gaajarla Sheshugoud <103266813+ShashiDivs@users.noreply.github.com>
2024-02-20 20:05:40 +0530

    downloaded data from GCP

---
+++ demo.py
@@ -0,0 +1,10 @@
+from hate.logger import logging
+from hate.exception import CustomException
+import os
+import sys
+from hate.configuration.gcloud_syncer import GCloudSync
+
+
+
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hate_speech_shashi","dataset.zip","download/dataset.zip")
